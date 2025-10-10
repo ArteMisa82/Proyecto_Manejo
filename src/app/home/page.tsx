52794472141fec64561a8d7d49cbd65cd7f3105d
Josue Guevara <el.jdragon@gmail.com>
2025-10-10 16:33:27 -0500

    Aumento de la tabla noticias en el eschema de prisma

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
 import Link from "next/link";
 import HardwareCarousel from "./components/CarrusellHome";
-import NewsSection from "./components/NewsSection"; 
+import NewsSection from "./components/NewsSection";
 import JuegosFranquicias from "./components/JuegosFranquicias";
+import { prisma } from "./src/lib/prisma";
 
 const games = [
   { id: 1, name: "Ghost of Tsushima", img: "/home/GHOST.png", offer: "-33%" },
@@ -13,12 +14,16 @@ const games = [
 ];
 
 const news = [
-  { id: 1, title: "Noticia 1", img: "/news/news1.jpg", date: "10 Oct", summary: "Resumen..." },
+  {
+    id: 1,
+    title: "Noticia 1",
+    img: "/news/news1.jpg",
+    date: "10 Oct",
+    summary: "Resumen...",
+  },
 ];
 
-const communities = [
-  { id: 1, name: "Comunidad A", members: 120 },
-];
+const communities = [{ id: 1, name: "Comunidad A", members: 120 }];
 
 export default function Home() {
   return (
@@ -26,36 +31,32 @@ export default function Home() {
       {/* Hero */}
       <div className="relative w-full h-[700px] overflow-hidden">
         <Link href="/juegos">
-        <Image 
-          src="/home/Banner_home.png"  
-          alt="Banner"
-          fill
-          className="object-cover"
-          priority                      
-        />
-         <div className="absolute inset-0 bg-black/40 flex items-center justify-center">
-          <h1 className="text-white text-4xl font-bold">Explora los nuevos lanzamientos</h1>
-        </div>
-
+          <Image
+            src="/home/Banner_home.png"
+            alt="Banner"
+            fill
+            className="object-cover"
+            priority
+          />
+          <div className="absolute inset-0 bg-black/40 flex items-center justify-center">
+            <h1 className="text-white text-4xl font-bold">
+              Explora los nuevos lanzamientos
+            </h1>
+          </div>
         </Link>
-       
       </div>
 
       {/* Juegos + Franquicias */}
-      <div className="font-sans"  ></div>
+      <div className="font-sans"></div>
       <JuegosFranquicias games={games} />
 
       <div className="font-sans">
-      {/* Carrusel Hardware */}
-      <HardwareCarousel />
+        {/* Carrusel Hardware */}
+        <HardwareCarousel />
       </div>
 
-       {/* Noticias + Próximos Estrenos */}
-      <NewsSection/>
-
-
-
+      {/* Noticias + Próximos Estrenos */}
+      <NewsSection />
     </div>
-   
   );
 }
